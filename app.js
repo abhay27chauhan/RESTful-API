42a35e1842c184f15b099ed9cefac5c1bfbe03af
Abhay <chauhan27.abhay@gmail.com>
2021-01-02 01:41:42 +0530

    wiki api

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const ejs = require("ejs");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
